ffb14f9b269a74a9b58a100665db0f512e3c9400
tron01 <jangoovasuu0789@gmail.com>
2022-10-30 21:19:18 +0530

    Earth 3d

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/R0i-jhvFXenbAJp8/scene.splinecode');
