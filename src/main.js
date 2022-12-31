94390314c72aa407d8eb33e22f59e548d47244ec
tron01 <69385258+tron01@users.noreply.github.com>
2022-12-31 20:25:55 +0530

    added camera and scene

--- src/main.js
+++ src/main.js
@@ -4,4 +4,23 @@ const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-console.log("hai");
+//console.log("hai");
+
+//scene
+const scene = new THREE.Scene();
+//camera(field of view,aspect,near,far)
+const camera = new THREE.PerspectiveCamera(75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000);
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+//camera position
+/*
+camera.position.z = 5;
+camera.position.y = 2;
+camera.position.x = 0;
+*/ camera.position.set(0, 2, 5);
+
+//render(scene,camera)
+renderer.render(scene, camera);    
