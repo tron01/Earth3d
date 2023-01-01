d67fb4d2fd40db85a2f0e5b8e69bb7219a824698
tron01 <69385258+tron01@users.noreply.github.com>
2023-01-01 20:15:16 +0530

    star mesh

--- src/main.js
+++ src/main.js
@@ -13,10 +13,8 @@ const camera = new THREE.PerspectiveCamera(75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000);
-//orbit controler
-const orbit = new OrbitControls(camera, renderer.domElement);
-/*axesHelper 
 
+/*axesHelper 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 */
@@ -27,8 +25,6 @@ camera.position.y = 2;
 camera.position.x = 0;
 */ camera.position.set(0, 2, 5);
 
-//camera postion update by mouse 
-orbit.update();
 
 //box
 const boxGeometry = new THREE.BoxGeometry();
@@ -37,12 +33,12 @@ const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
 //plane
-
+/*
 const planeGeometry = new THREE.PlaneGeometry(10, 10);
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xee000f })
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
-
+*/
 //point light
 /*
 const pointLight = new THREE.PointLight(0xffffff);
@@ -56,11 +52,27 @@ scene.add(AmbientLight);
 //gridHelper
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(gridHelper);
+//orbit controler
+const orbit = new OrbitControls(camera, renderer.domElement);
 
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
     //render(scene,camera)
     renderer.render(scene, camera);
+    //camera postion update by mouse 
+    orbit.update();
 }
+//star mesh
+function addStar() {
+    const SphereGeometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const SphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
+    const star = new THREE.Mesh(SphereGeometry, SphereMaterial);
+    //randmon star position
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+Array(200).fill().forEach(addStar);
+
 renderer.setAnimationLoop(animate)
